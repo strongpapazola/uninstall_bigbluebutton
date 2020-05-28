5908cddaf7f3f52e00e2ee49b1e45babd616a268
strongpapazola <strongpapazola@gmail.com>
2020-05-28 18:31:36 +0700

    modern

--- uninstall_package.py
+++ uninstall_package.py
@@ -2,6 +2,21 @@
 
 from os import system
 import subprocess
+import time
+
+a = '''
+__     ___     _ _     _
+\ \   / (_)___(_) |_  | |
+ \ \ / /| / __| | __| | |
+  \ V / | \__ \ | |_  |_|
+   \_/  |_|___/_|\__| (_)
+
+Author : https://github.com/strongpapazola/
+'''
+
+print(a)
+time.sleep(3)
+
 
 def shell(cmd):
  a = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
