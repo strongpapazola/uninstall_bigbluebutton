b6f3a530b1f8b85f840f85eb7f0e2d79ce2b6a3e
strongpapazola <strongpapazola@gmail.com>
2020-05-28 18:26:05 +0700

    modern

--- uninstall_package.py
+++ uninstall_package.py
@@ -1,16 +1,28 @@
+#tell me https://github.com/strongpapazola if this script error
+
 from os import system
 import subprocess
-a = subprocess.Popen('dpkg -l | grep bbb', shell=True, stdout=subprocess.PIPE).stdout
-a = str(a.read())
-a = a.split("'")[1]
-a = a.split('\\n')
-a = a[:-1]
+
+# old
+#a = subprocess.Popen('dpkg -l | grep bbb', shell=True, stdout=subprocess.PIPE).stdout
+#a = str(a.read())
+#a = a.split("'")[1]
+#a = a.split('\\n')
+#a = a[:-1]
+
+def shell(cmd):
+ a = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+ return a
+
+a = shell('dpkg -l | grep bbb').splitlines()
+
 
 j = []
 for i in a:
  i = i.split('  ')
  if i[1]:
-  system('apt purge '+i[1]+' -y')
+  print(i[1])
+#  system('apt purge '+i[1]+' -y')
 
-system('apt autoremove -y')
+#system('apt autoremove -y')
 
